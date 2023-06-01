4015407bc21705e74ee1c43c58f095e76e1f2d13
Eduard Peters <76105887+eduardpeters@users.noreply.github.com>
2023-06-01 15:54:23 +0200

    Add control structure

--- src/mealplanner/Main.java
+++ src/mealplanner/Main.java
@@ -1,10 +1,12 @@
 package mealplanner;
 
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+        ArrayList<Meal> meals = new ArrayList<>();
 
         while (true) {
             System.out.println("What would you like to do (add, show, exit)?");
@@ -13,8 +15,18 @@ public class Main {
             if (command.equals("exit")) {
                 break;
             }
+
+            switch (command) {
+                case "add" -> addMeal(scanner, meals);
+                case "show" -> showMeals(meals);
+                default -> {
+                }
+            }
         }
 
+    }
+
+    public static void addMeal(Scanner scanner, ArrayList<Meal> meals) {
         System.out.println("Which meal do you want to add (breakfast, lunch, dinner)?");
         String category = scanner.nextLine();
         System.out.println("Input the meal's name:");
@@ -23,12 +35,19 @@ public class Main {
         String allIngredients = scanner.nextLine();
         String[] ingredients = allIngredients.split(",");
 
-        Meal newMeal = new Meal(category, name, ingredients);
-
-        System.out.println();
-
-        newMeal.printMeal();
+        meals.add(new Meal(category, name, ingredients));
 
         System.out.println("The meal has been added!");
+    }
+
+    public static void showMeals(ArrayList<Meal> meals) {
+        if (meals.isEmpty()) {
+            System.out.println("No meals saved. Add a meal first.");
+        } else {
+            for (Meal meal : meals) {
+                System.out.println();
+                meal.printMeal();
+            }
+        }
     }
 }
